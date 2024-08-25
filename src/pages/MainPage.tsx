2905c123861c88b6f37810950eb0e6d0f93dfd80
2mingyu <tnt01010@naver.com>
2024-08-26 00:39:45 +0900

    feat: navbar

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,15 +1,12 @@
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 export default function MainPage() {
-  const navigate = useNavigate();
   return (
     <MainPageWrapper>
       <Outlet></Outlet>
-      <div onClick={() => navigate("/")}>홈 자리임 ㅋㅋ</div>
-      <div onClick={() => navigate("/community")}>커뮤니티 자리임 ㅋㅋ</div>
-      <div onClick={() => navigate("/mypage")}>마이페이지 자리임 ㅋㅋ</div>
+      <Navbar />
     </MainPageWrapper>
   );
 }
