38d767f8ae643b9d81b903720124e4aa1762a922
2mingyu <tnt01010@naver.com>
2024-10-08 05:06:34 +0900

    feat: 페이지 최초 접속 시 accessToken 확인, 등록된 유저가 아니면 /mypage로 이동 #79

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,9 +3,10 @@ import { useEffect } from "react";
 import { get_is_registered } from "../services/user";
 import { useNavigate } from "react-router-dom";
 
-// OAuth 로그인 후 url의 queryParam에서 token 가져오기 및 localStorage에 저장된 토큰 확인
 const useAuth = () => {
   const navigate = useNavigate();
+
+  // OAuth 로그인 후 url의 queryParam에서 token 가져오기 및 localStorage에 저장된 토큰 확인
   useEffect(() => {
     const authenticateUser = async () => {
       // URL의 queryParams에서 accessToken과 refreshToken 가져오기
@@ -39,6 +40,31 @@ const useAuth = () => {
 
     authenticateUser();
   }, [navigate]);
+
+  // 페이지 최초 접속 시 로컬 스토리지에 저장된 accessToken을 확인하고, get_is_registered 호출
+  useEffect(() => {
+    const checkAuth = async () => {
+      // localStorage에서 accessToken 가져오기
+      const accessToken = localStorage.getItem("accessToken");
+
+      // accessToken이 있을 경우 get_is_registered 호출
+      if (accessToken) {
+        try {
+          const response = await get_is_registered();
+
+          // 만약 유저가 등록되지 않았다면 /mypage로 이동
+          if (!response.data) {
+            navigate("/mypage");
+          }
+        } catch (error) {
+          console.error("Error checking registration status:", error);
+        }
+      }
+    };
+
+    checkAuth();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 };
 
 export default useAuth;
